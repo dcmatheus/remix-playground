1bc645e62b2a595a076b74965305d20f825230ab
dcmatheus <mathws.costag@gmail.com>
2022-09-12 18:29:06 -0300

    Configura axios

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,16 @@
-export default function Index() {
+import { useLoaderData } from "@remix-run/react";
+import api from "~/services/api";
+import { Container } from "./styles";
+
+export async function loader() {
+  const response = await api.get('0b498b01-c20f-49d9-8ab6-edda571b4d62');
+  return response.data.albums;
+}
+
+export default function Home() {
+  const albums = useLoaderData();
+  console.log(albums);
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Container>Olá</Container>
   );
 }
