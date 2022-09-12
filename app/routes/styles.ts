29504f3507a01f7f59f7a522f2f88852952f9134
dcmatheus <mathws.costag@gmail.com>
2022-09-12 18:28:57 -0300

    Cria o styled component Container

---
+++ app/routes/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  gap: 32px;
+
+  justify-content: center;
+  padding-top: 64px;
+`;
