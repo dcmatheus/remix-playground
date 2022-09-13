be4662e4f1efdef1737199bd2bbb638c63a12bca
dcmatheus <mathws.costag@gmail.com>
2022-09-12 23:31:28 -0300

    Cria a página inicial

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useNavigate } from "@remix-run/react";
 import api from "~/services/api";
-import { Container } from "./styles";
+import { Album, Container, TextContainer, Title, Image, Username } from "./styles";
 
 export async function loader() {
   const response = await api.get('0b498b01-c20f-49d9-8ab6-edda571b4d62');
@@ -9,8 +9,19 @@ export async function loader() {
 
 export default function Home() {
   const albums = useLoaderData();
-  console.log(albums);
+  const navigate = useNavigate();
   return (
-    <Container>Olá</Container>
+    <Container>
+      {albums.map((album: Album) => (
+        <Album key={album.id} onClick={() => navigate('details', { state: { album } })}>
+          <Image src={album.image} alt="Thumbnail" />
+          <TextContainer>
+            <Title>{album.title}</Title>
+            <Username>{album.username}</Username>
+          </TextContainer>
+        </Album>
+      ))
+    }
+    </Container>
   );
 }
